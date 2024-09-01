809987601aabb506a7cf3def703ff7ae5562679c
shivam gupta <guptashivam3935@gmail.com>
2024-09-01 22:24:09 +0530

    add 03propstailwind folder

--- 03propstailwind/src/App.jsx
+++ 03propstailwind/src/App.jsx
@@ -2,32 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <h1 className="text-3xl font-bold underline">
+    Hello world!
+    </h1>
+    <Card username="shivam" btnText="visit me " src="https://media.istockphoto.com/id/1372481295/photo/photo-of-school-boy-wear-yellow-t-shirt-backpack-in-background-stock-photo.jpg?s=1024x1024&w=is&k=20&c=tYIeshdaLJRFIlpE_FRonTchQi_Ps71xiPhGNLgCMb8="/>
+    <Card username="manish" btnText='go_to' src="https://media.istockphoto.com/id/1160527868/photo/young-indian-cute-child-with-book.jpg?s=1024x1024&w=is&k=20&c=ONnmgpnUj0wm8mihWcrPOtqxHCLw2oX9I9aZBtvdgGQ="/>
+
     </>
   )
 }
